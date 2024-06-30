6a88e03c9f07b75ce8baecee0fcaffe7d30f4d84
Sebastian Aybar <38281694+SebiAybar@users.noreply.github.com>
2024-06-30 19:58:17 +0200

    basic framework gui

--- Stundenzettel-Generator-kurzfristige-Arbeiter/src/main/java/com/example/application/StundenzettelGeneratorController.java
+++ Stundenzettel-Generator-kurzfristige-Arbeiter/src/main/java/com/example/application/StundenzettelGeneratorController.java
@@ -3,7 +3,7 @@ package com.example.stundenzettelgeneratorkurzfristigearbeiter;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class stundenzettelGeneratorController {
     @FXML
     private Label welcomeText;
 

439d2aff02475cf36c4534a8bb80acd4582f2ced
Sebastian Aybar <38281694+SebiAybar@users.noreply.github.com>
2024-09-02 17:48:47 +0200

    writeToExcel Methode in Einzelerstellung erweitert

--- Stundenzettel-Generator-kurzfristige-Arbeiter/src/main/java/com/ifi/application/Einzelerstellung.java
+++ Stundenzettel-Generator-kurzfristige-Arbeiter/src/main/java/com/ifi/application/Einzelerstellung.java
@@ -1,7 +1,18 @@
 package com.demo.application;
 
 
+import java.io.FileInputStream;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.demo.helper.Constants.PATH_FILE_STUNDENZETTEL_VORLAGE_LOCAL;
 
 public class Einzelerstellung {
 
@@ -18,8 +29,51 @@ public class Einzelerstellung {
     }
 
     public void writeToExcel(String outputPath, String lohn, boolean isErsetzenSelected) {
-        String resourceFilePath = "/Stundenzettel_Vorlage.xlsx";
+        try {
+            String resourceFilePath = "/Stundenzettel_Vorlage.xlsx";
+            InputStream resourceStream = Einzelerstellung.class.getResourceAsStream(resourceFilePath);
+
+            if (resourceStream == null) {
+                System.err.println("File not found in resources: " + resourceFilePath);
+                return;
+            }
+
+            // Create the destination path in the home directory
+
+            Path destinationPath = Paths.get(PATH_FILE_STUNDENZETTEL_VORLAGE_LOCAL);
+
+            if (!Files.exists(destinationPath)) {
+                // Copy the file from resources to the home directory
+                Files.copy(resourceStream, destinationPath, StandardCopyOption.REPLACE_EXISTING);
+                System.out.println("Stundenzettel_Vorlage wurde kopiert zu" + destinationPath);
+            } else {
+                System.out.println("Vorlage existiert bereits im Verzeichnis: " + PATH_FILE_STUNDENZETTEL_VORLAGE_LOCAL);
+            }
+
+            InputStream inputStream = new FileInputStream(PATH_FILE_STUNDENZETTEL_VORLAGE_LOCAL);
+            Workbook workbook = WorkbookFactory.create(inputStream);
+            Sheet currentSheet = workbook.getSheetAt(0);
+
+            List<Cell> arbeitszeitenCells = new ArrayList<>();
+            String[] datum = abrechnungsmonat.split("/");
+            List<LocalDate> datenDesMonats = getDatenDesMonats(datum);
+            List<Row> rowsToRemove = new ArrayList<>();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
+    private List<LocalDate> getDatenDesMonats(String[] datum) {
+        YearMonth jahrMonat = YearMonth.of(Integer.parseInt(datum[0]), Integer.parseInt(datum[1]));
+        int anzahlTageImMonat = jahrMonat.lengthOfMonth();
+        LocalDate ersterTag = jahrMonat.atDay(1);
+        List<LocalDate> datenDesMonats = new ArrayList<>();
+        for (int j = 0; j < anzahlTageImMonat; j++) {
+            LocalDate aktuellesDatum = ersterTag.plusDays(j);
+            datenDesMonats.add(aktuellesDatum);
+        }
+        return datenDesMonats;
+    }
 }

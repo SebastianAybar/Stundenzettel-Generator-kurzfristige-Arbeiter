0a14f6debd3ce05caf00b6e37ce730e542e57b36
mos-gun <mosgun.mahdere@outlook.de>
2024-10-08 17:28:19 +0200

    zwischenstand

--- Stundenzettel-Generator-kurzfristige-Arbeiter/src/main/java/com/ifi/application/StundenzettelGeneratorController.java
+++ Stundenzettel-Generator-kurzfristige-Arbeiter/src/main/java/com/ifi/application/StundenzettelGeneratorController.java
@@ -520,9 +520,9 @@ public class StundenzettelGeneratorController implements Initializable {
         createIfNotExistingLocalFileLogo();
         createIfNotExistingLocalFileStundenzettelVorlage();
 
-//        textfieldInputPath.setText("C:\\Users\\MM\\Desktop\\neue_excel_kurzarbeiter\\KFB_0124.xlsx");
-//        textfieldOutputPath.setText("C:\\Users\\MM\\Desktop\\neue_excel_kurzarbeiter\\generierte_Dateien");
-        textfieldInputPath.setText("C:\\Users\\sebas\\OneDrive\\Dokumente\\GitHub\\Stundenzettel-Generator-kurzfristige-Arbeiter\\Dateien\\KFB_0124.xlsx");
-        textfieldOutputPath.setText("C:\\Users\\sebas\\OneDrive\\Dokumente\\GitHub\\Stundenzettel-Generator-kurzfristige-Arbeiter\\Dateien\\test output");
+        textfieldInputPath.setText("C:\\Users\\MM\\Desktop\\neue_excel_kurzarbeiter\\KFB_0124.xlsx");
+        textfieldOutputPath.setText("C:\\Users\\MM\\Desktop\\neue_excel_kurzarbeiter\\generierte_Dateien");
+//        textfieldInputPath.setText("C:\\Users\\sebas\\OneDrive\\Dokumente\\GitHub\\Stundenzettel-Generator-kurzfristige-Arbeiter\\Dateien\\KFB_0124.xlsx");
+//        textfieldOutputPath.setText("C:\\Users\\sebas\\OneDrive\\Dokumente\\GitHub\\Stundenzettel-Generator-kurzfristige-Arbeiter\\Dateien\\test output");
     }
 }
